48574d7e9f30e82c882e9e0b0e40d384528b4bbe
d <d.g.feat.th2iinchoiincho@gmail.com>
2012-03-25 03:07:31 +0900

    boostのincludeを""に変更

--- init_with_tuple/init_with_tuple.hpp
+++ init_with_tuple/init_with_tuple.hpp
@@ -1,11 +1,11 @@
 #include <utility>
 #include <tuple>
  
-#include <boost/preprocessor/tuple/elem.hpp>
-#include <boost/preprocessor/tuple/eat.hpp>
-#include <boost/preprocessor/tuple/rem.hpp>
-#include <boost/preprocessor/seq/for_each_i.hpp>
-#include <boost/preprocessor/facilities/is_empty.hpp>
+#include "boost/preprocessor/tuple/elem.hpp"
+#include "boost/preprocessor/tuple/eat.hpp"
+#include "boost/preprocessor/tuple/rem.hpp"
+#include "boost/preprocessor/seq/for_each_i.hpp"
+#include "boost/preprocessor/facilities/is_empty.hpp"
  
 #define INIT_WITH_TUPLE(seq, expr) INIT_WITH_TUPLE_I(seq, expr, BOOST_PP_CAT(init_with_tuple_tmp_, __LINE__),)
 #define INIT_WITH_TUPLE_AUTO(seq, expr) INIT_WITH_TUPLE_I(seq, expr, BOOST_PP_CAT(init_with_tuple_tmp_, __LINE__), auto)
@@ -18,4 +18,3 @@
 #define INIT_WITH_TUPLE_M_II(tmp, type, i, elem) \
     BOOST_PP_IIF(BOOST_PP_IS_EMPTY(elem), BOOST_PP_TUPLE_EAT(1), ; type elem) \
     (std::get<i>(std::forward<decltype((tmp))>(tmp)))
-    
