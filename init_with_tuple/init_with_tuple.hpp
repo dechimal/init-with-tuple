d779e1d787bca6abf559c5662e6076626375034d
d <d.g.feat.th2iinchoiincho@gmail.com>
2012-03-25 05:30:04 +0900

    カッコで括られていない剥き出しカンマ対応と任意の式への代入サポート追加
    カッコで括られていない剥き出しカンマ対応
    任意の式への代入サポート追加
    初期化と代入の混合をサポートしつつINIT_WITH_TUPLE_AUTOを実装する方法を思いつかなかったので削除
    （変数の型を自動決定するのは依然INIT_WITH_TUPLEの変数リストにauto xとか書けばできる）

--- init_with_tuple/init_with_tuple.hpp
+++ init_with_tuple/init_with_tuple.hpp
@@ -1,20 +1,43 @@
 #include <utility>
-#include <tuple>
  
 #include "boost/preprocessor/tuple/elem.hpp"
 #include "boost/preprocessor/tuple/eat.hpp"
 #include "boost/preprocessor/tuple/rem.hpp"
 #include "boost/preprocessor/seq/for_each_i.hpp"
 #include "boost/preprocessor/facilities/is_empty.hpp"
+
+namespace init_with_tuple {
+
+struct ignore_t {
+    template<typename T> ignore_t & operator=(T&&) const {}
+};
+
+constexpr ignore_t ignore{};
+
+}
+
+#define INIT_WITH_TUPLE(tupseq, ...) INIT_WITH_TUPLE_I(INIT_WITH_TUPLE_TO_SEQ(tupseq), (__VA_ARGS__), BOOST_PP_CAT(init_with_tuple_tmp_, __LINE__))
+#define INIT_WITH_TUPLE_I(seq, expr, tmp) \
+    auto && tmp = expr \
+    BOOST_PP_SEQ_FOR_EACH_I(INIT_WITH_TUPLE_M, tmp, seq)
  
-#define INIT_WITH_TUPLE(seq, expr) INIT_WITH_TUPLE_I(seq, expr, BOOST_PP_CAT(init_with_tuple_tmp_, __LINE__),)
-#define INIT_WITH_TUPLE_AUTO(seq, expr) INIT_WITH_TUPLE_I(seq, expr, BOOST_PP_CAT(init_with_tuple_tmp_, __LINE__), auto)
-#define INIT_WITH_TUPLE_I(seq, expr, tmp, type) \
-    auto && tmp = expr; \
-    BOOST_PP_SEQ_FOR_EACH_I(INIT_WITH_TUPLE_M, (tmp, type), seq)
- 
-#define INIT_WITH_TUPLE_M(r, data, i, elem) INIT_WITH_TUPLE_M_I(BOOST_PP_TUPLE_REM(2) data, i, elem)
-#define INIT_WITH_TUPLE_M_I(data, i, elem) INIT_WITH_TUPLE_M_II(data, i, elem)
-#define INIT_WITH_TUPLE_M_II(tmp, type, i, elem) \
-    BOOST_PP_IIF(BOOST_PP_IS_EMPTY(elem), BOOST_PP_TUPLE_EAT(1), ; type elem) \
-    (std::get<i>(std::forward<decltype((tmp))>(tmp)))
+#define INIT_WITH_TUPLE_M(r, tmp, i, elem) \
+    BOOST_PP_IIF(INIT_WITH_TUPLE_IS_PAREN(elem), INIT_WITH_TUPLE_M_I, BOOST_PP_TUPLE_EAT(3))(tmp, i, BOOST_PP_TUPLE_REM(1) elem)
+#define INIT_WITH_TUPLE_M_I(tmp, i, elem) INIT_WITH_TUPLE_M_II(tmp, i, elem)
+#define INIT_WITH_TUPLE_M_II(tmp, i, ...) \
+    ; __VA_ARGS__ = std::get<i>(std::forward<decltype((tmp))>(tmp))
+
+#define INIT_WITH_TUPLE_TO_SEQ(tupseq) BOOST_PP_CAT(INIT_WITH_TUPLE_PACK_A tupseq, _END)
+#define INIT_WITH_TUPLE_PACK_A(...) ((__VA_ARGS__)) INIT_WITH_TUPLE_PACK_B
+#define INIT_WITH_TUPLE_PACK_B(...) ((__VA_ARGS__)) INIT_WITH_TUPLE_PACK_A
+#define INIT_WITH_TUPLE_PACK_A_END
+#define INIT_WITH_TUPLE_PACK_B_END
+
+#define INIT_WITH_TUPLE_IS_PAREN(...) BOOST_PP_TUPLE_ELEM(0, (INIT_WITH_TUPLE_CAT(INIT_WITH_TUPLE_IS_PAREN_CHECK_, INIT_WITH_TUPLE_IS_PAREN_HELPER __VA_ARGS__)))
+#define INIT_WITH_TUPLE_IS_PAREN_HELPER(...) INIT_WITH_TUPLE_IS_PAREN_HELPER_I tokens
+
+#define INIT_WITH_TUPLE_IS_PAREN_CHECK_INIT_WITH_TUPLE_IS_PAREN_HELPER 0,
+#define INIT_WITH_TUPLE_IS_PAREN_CHECK_INIT_WITH_TUPLE_IS_PAREN_HELPER_I 1,
+
+#define INIT_WITH_TUPLE_CAT(x, y, ...) INIT_WITH_TUPLE_CAT_I(x, y)
+#define INIT_WITH_TUPLE_CAT_I(x, y) x ## y
--- test.cpp
+++ test.cpp
@@ -1,12 +1,18 @@
 #include "init_with_tuple/init_with_tuple.hpp"
+#include <tuple>
 #include <iostream>
+#include <string>
+
+template<typename, typename>
+using hoge = int;
 
 int main()
 {
-    INIT_WITH_TUPLE((const int x)(const int y)(const int z), std::make_tuple(1, 2, 3));
+    using init_with_tuple::ignore;
+    INIT_WITH_TUPLE((int x)(ignore)(auto z), std::make_tuple(1, 2, std::string("hoge")));
+    std::cout << x << "," << z << std::endl;
+
+    INIT_WITH_TUPLE((x)(hoge<int, int> y)(z), std::make_tuple<int, int, std::string>(3, 4, "fuga"));
     std::cout << x << "," << y << "," << z << std::endl;
- 
-    INIT_WITH_TUPLE_AUTO((a)()(str), std::make_tuple(4, 5, "hoge"));
-    std::cout << a << "," << str << std::endl;
     return 0;
 }
